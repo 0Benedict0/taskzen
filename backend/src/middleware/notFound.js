f184b4fe35f08f3d84147a090c09063f8b1c0495
Vlad <vladkorinenko123@gmail.com>
2026-08-21 09:44:48 +0300

    Fix GitHub Pages routing and file

--- backend/src/middleware/notFound.js
+++
@@ -1,7 +0,0 @@
-const notFound = (req, res) => {
-  res.status(404).json({
-    message: `Маршрут '${req.originalUrl}' не знайдено`,
-  });
-};
-
-export default notFound;
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/taskzen">
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
 );
